11b0aeae4199ba7626b5f221387cc80cbd7a011c
Ry <57374659+cryptidgit@users.noreply.github.com>
2022-05-25 16:29:03 -0400

    Removed requireJS

--- dist/en-session-attribution.js
+++ dist/en-session-attribution.js
@@ -1,5 +1,4 @@
-"use strict";
-const { v4: uuidv4 } = require('uuid');
+import { v4 as uuidv4 } from 'uuid';
 function getCookie(cookie) {
     const cookies = document.cookie;
     const cookiesArr = cookies.split("; ");
